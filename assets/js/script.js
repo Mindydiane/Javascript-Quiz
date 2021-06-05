95408b81468e1520b2cb5b0ebe278da9eea2002b
unknown <mindydiane@gmail.com>
2021-06-04 22:24:21 -0500

    JS initial structure setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
+//stored variables from html id
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
 //function to generate quiz
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     function showQuestions(questions, quizContainer){
+        console.log('generateQuiz');
+        console.log('showResults');
         // code will go here
     }    
 
@@ -17,3 +24,17 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     showResults(questions,quizContainer, resultsContainer);
     }
 }
+
+var myQuestions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: '<script>',
+            b: '<scripting>',
+            c: '<js>',
+            d: '<javascript>'
+        },
+        correctAnswer: 'a'
+
+    },
+]
